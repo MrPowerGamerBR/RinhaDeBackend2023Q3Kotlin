00fc5efa9b566b84f5dc293d523c7ac796bf00ee
MrPowerGamerBR <git@mrpowergamerbr.com>
2023-08-26 22:15:34 -0300

    Log available processors

--- src/main/kotlin/com/mrpowergamerbr/rinhadebackend2023q3/RinhaDeBackend2023Q3Launcher.kt
+++ src/main/kotlin/com/mrpowergamerbr/rinhadebackend2023q3/RinhaDeBackend2023Q3Launcher.kt
@@ -14,6 +14,7 @@ object RinhaDeBackend2023Q3Launcher {
         logger.info { "OLÁ E SEJAM BEM-VINDOS A RINHA DE BACKEND 2023 Q3™" }
         logger.info { "*música épica de fundo*" }
         logger.info { "Implementação feita por MrPowerGamerBR" }
+        logger.info { "Available Processors: ${Runtime.getRuntime().availableProcessors()}" }
 
         val database = DatabaseStuff.createPostgreSQLPudding(
             System.getenv("POSTGRESQL_ADDRESS"),
